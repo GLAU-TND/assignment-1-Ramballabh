1478c3e8fb383c382c9fa46e260097f172439450
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-20 18:53:16 +0530

    declare the object of list

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,9 +1,13 @@
 package person;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
     private String fName;
     private String lName;
     private String email;
     private long phoneNumber;
+    List<Long> list = new ArrayList<>();
 
 }
