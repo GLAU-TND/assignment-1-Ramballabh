b96e123b1aee55749735be3e1de14631904bad5a
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-20 18:54:05 +0530

    define the constructor in Person.java

--- src/person/Person.java
+++ src/person/Person.java
@@ -10,4 +10,12 @@ public class Person {
     private long phoneNumber;
     List<Long> list = new ArrayList<>();
 
+    public Person(String fName, String lName, String email, long phoneNumber, List<Long> list) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.list = list;
+    }
+
 }
