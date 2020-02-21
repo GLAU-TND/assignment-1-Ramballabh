3b882b507ebd32335a554b902bbd9f4a0d721651
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-21 14:10:27 +0530

    implement the interface SinglyADT

--- src/definition/SinglyLinkedLIst.java
+++ src/definition/SinglyLinkedLIst.java
@@ -1,4 +1,30 @@
 package definition;
 
-public class SinglyLinkedLIst<E> {
+import adt.SinglyADT;
+
+public class SinglyLinkedLIst<E> implements SinglyADT<E> {
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
