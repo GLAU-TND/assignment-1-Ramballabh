c5c895f264e833642cd2dddc5b3d506872beb4f5
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-21 14:13:45 +0530

    create constructor of node class

--- src/definition/SinglyLinkedLIst.java
+++ src/definition/SinglyLinkedLIst.java
@@ -29,8 +29,13 @@ public class SinglyLinkedLIst<E> implements SinglyADT<E> {
     }
 
     private static class Node<E> {
-        private E dtaa;
+        private E data;
         private Node<E> next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
 
 
     }
